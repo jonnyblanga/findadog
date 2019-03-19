f804eb7e4eb2223cac7eb7595653ba40bfa8f627
Jonathan Blanga <jonathanblanga@gmail.com>
2019-03-19 15:56:32 +0100

    changed title name in application.html.erb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+
+puts 'Cleaning database...'
+User.destroy_all
+Dog.destroy_all
+Message.destroy_all
+Favorite.destroy_all
