f6a35e0c544f216b3135ca70de85104ab74c8b24
silvia sartor <silvia.maria.sartor@gmail.com>
2019-03-19 17:09:02 +0100

    fixed the cloudinary uploader

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,14 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'about', to: 'pages#about', as: :about
+
+  get 'dogs/me', to: 'dogs#me', as: :me
+
+  resources :dogs do
+    resources :favorites, only: [:create, :destroy]
+  end
+
+  resources :favorites, only: [:index]
+  resources :messages, only: [:new, :create, :destroy]
 end
