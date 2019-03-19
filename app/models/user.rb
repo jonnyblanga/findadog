7302b89dc157afa49342baf3972f29a15849fc36
Ivan Radeljevic <ivan.radeljevic98@gmail.com>
2019-03-19 19:11:54 +0100

    validations done

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :name, presence: true, if: :verify_shelter
+  validates :phone, presence: true, format: { with: /\S+/}, if: :verify_shelter
+  validates :address, presence: true, if: :verify_shelter
+  validates :name, presence: true, if: :verify_user
+
+  def verify_shelter
+    self.role == 'shelter'
+  end
+
+  def verify_user
+    self.role == 'user'
+  end
 end
