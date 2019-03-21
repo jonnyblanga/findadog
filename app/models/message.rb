9f0925ee1ea6c8bf26c980f57a308dd844e00d99
Jonathan Blanga <jonathanblanga@gmail.com>
2019-03-21 16:00:02 +0100

    messages controller

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,4 +2,12 @@ class Message < ApplicationRecord
   validates :context, length: { in: 1..500 }
   validates :from_user_id, presence: true
   validates :to_user_id, presence: true
+
+  def sender
+    User.find(self.from_user_id)
+  end
+
+  def receiver
+    User.find(self.to_user_id)
+  end
 end
